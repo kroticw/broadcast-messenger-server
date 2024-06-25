49ea154b2710be05defcd9a5ea6e2629094bef78
krotic <denises.krotic@ya.ru>
2024-06-26 00:24:50 +0500

    рабочая версия

--- cmd/communication.go
+++ cmd/communication.go
@@ -4,15 +4,22 @@ import (
 	"context"
 	"encoding/binary"
 	"encoding/json"
+	"fmt"
 	"github.com/sirupsen/logrus"
+	"io"
+	"os"
 	"strconv"
+	"strings"
 	"time"
 )
+
+var itsFile = false
 
 func (c *Client) handleTunnelClient(ctx context.Context, clients *AtomicClientsMap) {
 	defer c.Close(clients)
 	_, exist := clients.Get(c.Username)
 	logrus.Println("exist: ", exist)
+	quit := make(chan bool)
 	if !exist {
 		clients.Push(c)
 		logrus.WithFields(logrus.Fields{
@@ -40,7 +47,8 @@ func (c *Client) handleTunnelClient(ctx context.Context, clients *AtomicClientsM
 				}
 			}
 		}
-		go c.meInOnline(clients)
+
+		go c.meInOnline(clients, quit)
 	}
 	for {
 		var message TcpMessage
@@ -76,38 +84,28 @@ func (c *Client) handleTunnelClient(ctx context.Context, clients *AtomicClientsM
 					"error": err,
 				})
 				c.Close(clients)
+				return
 			}
-			return
 		}
 
 	}
 }
 
 func (c *Client) situationFile(message *TcpMessage, clients *AtomicClientsMap) error {
-	length, err := strconv.Atoi(message.ServiceData)
+	file, err := os.Create(strings.TrimSpace(message.Message))
 	if err != nil {
-		logrus.WithFields(logrus.Fields{
-			"error":    err,
-			"ip":       c.ClientIP,
-			"username": c.Username,
-		}).Errorln("error convert to int file length")
 		return err
 	}
-	fileBuf := make([]byte, length)
-    totalRead := 0
-    for totalRead < length {
-        n, err := c.Conn.Read(fileBuf[totalRead:])
-        if err != nil {
-            logrus.WithFields(logrus.Fields{
-                "error":    err,
-                "ip":       c.ClientIP,
-                "username": c.Username,
-            }).Errorln("error receive and read file")
-            return err
-        }
-        totalRead += n
-    }
-
+	length, err := strconv.Atoi(message.ServiceData)
+	if err != nil {
+		return err
+	}
+	n, err := io.CopyN(file, c.Conn, int64(length))
+	if err != nil {
+		return err
+	}
+	fmt.Println(n, " bytes sent")
+	file.Close()
 	newMes := &TcpMessage{
 		From:        message.From,
 		To:          message.To,
@@ -115,51 +113,51 @@ func (c *Client) situationFile(message *TcpMessage, clients *AtomicClientsMap) e
 		ServiceData: message.ServiceData,
 		Message:     message.Message,
 	}
-	err := c.sendTo(newMes, clients)
+	err = c.sendTo(newMes, clients)
 	if err != nil {
 		logrus.WithFields(logrus.Fields{
 			"error": err,
 		}).Errorln("error sending message")
 		return err
 	}
-	go c.sendFileTo(message.To, fileBuf, clients)
-	//if err != nil {
-	//	logrus.WithFields(logrus.Fields{
-	//		"error": err,
-	//	}).Errorln("error sending message")
-	//	return err
-	//}
+	time.Sleep(100 * time.Millisecond)
+
+	c.sendFileTo(message, clients)
 	return nil
 }
 
 // функция, которая раз в 5 секунд отправляет всем в clients 2 сообщения - размер json и json, что она новый юзер
-func (c *Client) meInOnline(clients *AtomicClientsMap) {
-	defer c.Close(clients)
+func (c *Client) meInOnline(clients *AtomicClientsMap, quit chan bool) {
+	//defer c.Close(clients)
 	for {
-
-		mp := clients.GetMap()
-		for _, client := range mp {
-			if client.Username != c.Username {
-				var newClientMes TcpMessage
+		select {
+		case <-quit:
+			return
+		default:
+			mp := clients.GetMap()
+			for _, client := range mp {
 				if client.Username != c.Username {
-					newClientMes = TcpMessage{
-						From:        "server",
-						To:          client.Username,
-						ServiceType: "new_user",
-						ServiceData: c.Username,
-					}
+					var newClientMes TcpMessage
+					if client.Username != c.Username {
+						newClientMes = TcpMessage{
+							From:        "server",
+							To:          client.Username,
+							ServiceType: "new_user",
+							ServiceData: c.Username,
+						}
 
-					err := c.sendTo(&newClientMes, clients)
-					if err != nil {
-						logrus.WithFields(logrus.Fields{
-							"error": err,
-						}).Errorln("error sending message")
-						return
+						err := c.sendTo(&newClientMes, clients)
+						if err != nil {
+							logrus.WithFields(logrus.Fields{
+								"error": err,
+							}).Errorln("error sending message")
+							return
+						}
 					}
 				}
 			}
+			time.Sleep(10 * time.Second)
 		}
-		time.Sleep(60 * time.Second)
 	}
 }
 
@@ -195,21 +193,34 @@ func (c *Client) sendTo(message *TcpMessage, clients *AtomicClientsMap) error {
 	return nil
 }
 
-func (c *Client) sendFileTo(toUsername string, fileBuf []byte, clients *AtomicClientsMap) {
-	client, exist := clients.Get(toUsername)
+func (c *Client) sendFileTo(message *TcpMessage, clients *AtomicClientsMap) error {
+	logrus.WithFields(logrus.Fields{
+		"to": message.To,
+	}).Println("start sending file")
+	client, exist := clients.Get(message.To)
 	if !exist {
 		logrus.Errorln("client not found for sending file")
-		return
+		return nil
 	}
-	totalWritten := 0
-    while totalWritten < len(fileBuf) {
-        n, err := client.Conn.Write(fileBuf[totalWritten:])
-        if err != nil {
-            logrus.WithFields(logrus.Fields{
-                "error": err,
-            }).Errorln("error sending file")
-            return
-        }
-        totalWritten += n
-    }
+	file, err := os.Open(strings.TrimSpace(message.Message))
+	if err != nil {
+		return err
+	}
+
+	length, err := strconv.Atoi(message.ServiceData)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+	n, err := io.CopyN(file, client.Conn, int64(length))
+	if err != nil {
+		return err
+	}
+	logrus.WithFields(logrus.Fields{
+		"message": message,
+		"to":      message.To,
+		"size":    n,
+	}).Println("send file to client")
+	return nil
 }
